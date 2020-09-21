f84d7f5091e2834bcc18f5b2e7998f9abde6afe4
Ujjal Acharya <ujjal.acharya@amniltech.com>
2020-09-21 21:02:40 +0545

    move files to a timestamp folder

--- index.js
+++ index.js
@@ -3,13 +3,16 @@ const fs = require("fs");
 const request = require("request");
 
 (async () => {
+
   // Take argument from CLI
   let argument = process.argv[2];
-  const browser = await puppeteer.launch({ headless: true });
+  const browser = await puppeteer.launch({ headless: false });
   const page = await browser.newPage();
   await page.goto(argument.toString());
 
   let doc = await page.evaluate(async () => {
+
+    // Take only tags with img attribute
     let images = document.getElementsByTagName("img");
 
     let imagesArr = [...images];
@@ -26,34 +29,28 @@ const request = require("request");
   });
 
   var download = function (uri, filename, callback) {
-    console.log({ uri });
     request.head(uri, function (err, res, body) {
-      console.log("content-type:", res.headers["content-type"]);
-      console.log("content-length:", res.headers["content-length"]);
       request(uri).pipe(fs.createWriteStream(filename)).on("close", callback);
     });
   };
 
-  //   console.log(doc)
-
   let timestamp = Date.now();
+  timestamp = timestamp.toString();
 
-  fs.mkdir(timestamp.toString(), { recursive: true }, (err) => {
+  //   Make a directory with timestamp's file name
+  fs.mkdir(timestamp, { recursive: true }, (err) => {
     if (err) throw err;
   });
 
+  //   Download and move files to the created directory
   doc.forEach((item, i) => {
     download(item, `${i}.jpg`, function () {
-        // fs.copy(`./${i}.jpg`, './${timestamp}', function (err) {
-        //     if (err) {
-        //       console.error(err);
-        //     } else {
-        //       console.log("success!");
-        //     }
-        //   }); //copies directory, even if it has subdirectories or files
+      fs.rename(`./${i}.jpg`, `./${timestamp}/${i}.jpg`, function (err) {
+        if (err) throw err;
+        console.log("Successfully downloaded...");
+      });
     });
   });
-
 
   await browser.close();
 })();
